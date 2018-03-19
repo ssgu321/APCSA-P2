d65b38658c984e5c68edc505bc7dbb06f6d81cdd
Sonia Gu <ssgu321@gmail.com>
2018-03-19 11:08:58 -0700

    Unit 12

--- Unit12/src/Lab18d/Lab18d.java
+++ Unit12/src/Lab18d/Lab18d.java
@@ -9,6 +9,7 @@ package Lab18d;
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.Arrays;
 import static java.lang.System.*;
 
@@ -16,18 +17,40 @@ public class Lab18d
 {
 	public static void main( String args[] ) throws IOException
 	{
+		ArrayList<Word> unsorted = new ArrayList<Word>();
+		
 		Scanner file = new Scanner(new File("H:\\APCS\\Unit12-2016\\Unit12-Assignments\\lab18d\\lab18d.dat"));
 
 		int size = file.nextInt();
-		file.nextLine();
 		
-
-
-
-
-
-
-
+		while(file.hasNext())
+		{
+			Word test = new Word(file.nextLine());
+			unsorted.add(test);
+			
+		}
+		
+		unsorted.remove(0);
+		out.println(unsorted);	
+		Word min = new Word("gekkkiiisfgsfgsdfgsgs");
+		
+		int ind = 0;
+		for (int j = 0 ; j < size; j++)
+		{
+			for (int i = 0; i < unsorted.size() ; i++)
+			{
+				if (unsorted.get(i).compareTo(min) == -1)
+				{
+					min  = unsorted.get(i);
+					ind = i;
+				}
+			}
+			out.println(min);
+			min = new Word("gekkkiiisfgfgfsgss");
+			unsorted.remove(ind);
+			
+		}
+		
 
 
 
--- Unit12/src/Unit18e/Word.java
+++ Unit12/src/Unit18e/Word.java
@@ -1,3 +1,5 @@
+package Unit18e;
+
 //© A+ Computer Science  -  www.apluscompsci.com
 //Name -
 //Date -
@@ -12,31 +14,43 @@ public class Word implements Comparable<Word>
 
 	public Word( String s)
 	{
-
-
+		word = s;
 	}
 
-	private int numVowels()
+	int numVowels()
 	{
 		String vowels = "AEIOUaeiou";
 		int vowelCount=0;
-
-
-
-
-
-
-
+		
+	
+		for (int i = 0; i < word.length(); i++)
+		{
+			for (int j = 0; j < vowels.length(); j++)
+			{
+				if(word.substring(i, i+1).equals(vowels.substring(j, j+1)))
+				{
+					vowelCount++;
+				}
+			}
+		}
+		
 		return vowelCount;
 	}
 
 	public int compareTo(Word rhs)
 	{
+		if (word.equals(rhs.toString()))
+			return 0;
+		else if (numVowels() < rhs.numVowels())
+			return -1; 
+		else if (numVowels() == rhs.numVowels())
+		{
+			return 0; 
+		}	
+		
+		
+		return 1;
 
-
-
-	
-		return -1;
 	}
 
 	public String toString()
